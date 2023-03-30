eca8154102b14103f2319a484b873beb9fabf727
Bri <duvivierbri@gmail.com>
2023-03-29 22:39:08 -0400

    Small change

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -4,11 +4,6 @@ import ProjectTitle from '@/components/projectPage/projectTitle'
 import NavigationBar from '@/components/sections/nav'
 import { createClient } from 'contentful'
 
-var client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-})
-
 export default function ProjectDetailPage({ project }: any) {
   const { title, about, technologyStack } = project[0].fields
 
@@ -25,6 +20,11 @@ export default function ProjectDetailPage({ project }: any) {
 }
 
 export async function getStaticPaths() {
+  var client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  })
+
   const res = await client.getEntries({ content_type: 'portfolioProject' })
 
   const paths = res.items.map((project: any) => ({
@@ -37,7 +37,12 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }: any) {
+  var client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  })
+
   const { items } = await client.getEntries({
     content_type: 'portfolioProject',
     'fields.slug': params.id,
