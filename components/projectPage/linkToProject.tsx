b04b62213e5b0eb296f781ac77ad59ed8e4a0f9b
Bri <duvivierbri@gmail.com>
2023-04-03 21:25:05 -0400

    Centered project link button

--- components/projectPage/linkToProject.tsx
+++ components/projectPage/linkToProject.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 
 export default function LinkToProject({ link }: any) {
   return (
-    <Link
-      className="w-fit h-fit p-1 rounded-lg border border-2 border-white hover:bg-white hover:text-black"
-      target={'_blank'}
-      href={link}
-    >
-      Check Out The Project Here!
-    </Link>
+    <div className="flex w-full justify-center items-center my-5">
+      <Link
+        className="w-fit h-fit p-1 rounded-lg border border-2 border-white hover:bg-white hover:text-black"
+        target={'_blank'}
+        href={link}
+      >
+        Check Out The Project Here!
+      </Link>
+    </div>
   )
 }
