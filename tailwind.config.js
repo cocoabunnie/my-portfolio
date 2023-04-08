8ff7f12aae33de6ffdd1b9c8185c2e51d4f8131e
Bri <duvivierbri@gmail.com>
2023-04-07 22:27:48 -0400

    Added header animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,32 @@ module.exports = {
       black: '#000000',
       transparent: 'transparent',
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        slideInFromLeft: {
+          '0%': {
+            transform: 'translateX(-100%)',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+          },
+        },
+        slideInFromRight: {
+          '0%': {
+            transform: 'translateX(100%)',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+          },
+        },
+      },
+      animation: {
+        slideInFromLeftFast: 'slideInFromLeft 1.5s ease-out',
+        slideInFromLeftMedium: 'slideInFromLeft 1.7s ease-out',
+        slideInFromLeftSlow: 'slideInFromLeft 1.9s ease-out',
+        slideInFromRight: 'slideInFromRight 1s ease-out',
+      },
+    },
   },
   plugins: [],
 }
