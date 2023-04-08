4c903e6626621ff1d0d8214a3fa024e6585f0a51
Bri <duvivierbri@gmail.com>
2023-04-07 21:57:17 -0400

    Added new image and responsiveness wip

--- components/projectSquare.tsx
+++ components/projectSquare.tsx
@@ -4,7 +4,7 @@ export default function ProjectSquare({ imageSrc, slug }: any) {
   return (
     <Link
       href={'/project/' + slug}
-      className="cursor-pointer w-[calc(70vw)] lg:w-[calc(45vw)] hover:opacity-50"
+      className="cursor-pointer w-[calc(90vw)] lg:w-[calc(45vw)] hover:opacity-50"
     >
       <img
         className="border border-pink border-4 rounded-lg hover:border-pink"
--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -1,19 +1,21 @@
+import Image from 'next/image'
+
 export default function Header() {
   return (
     <div className="relative flex font-mono w-screen h-screen text-white">
-      <div className="absolute w-full left-0 top-60 pl-32 py-16 text-left bg-white text-black">
-        <p className="font-bold text-pink text-[calc(5vw)] uppercase tracking-widest">
+      <div className="w-full items-center absolute left-0 top-60 pl-5 md:pl-32 py-16 text-left bg-white text-black">
+        <p className="font-bold text-pink text-[calc(8vw)] md:text-[calc(5vw)] uppercase tracking-widest">
           Brianna Duvivier
         </p>
-        <p className="text-[calc(1.5vw)] mb-4">
+        <p className="text-[calc(2.5vw)] md:text-[calc(1.5vw)] mb-4">
           Full Stack Developer, creative technologist and
           <br />
           dog enthusiast
         </p>
 
-        <div className="flex flex-row gap-4 my-3">
+        <div className="flex flex-row justify-start gap-4 my-3">
           <a
-            className="border border-black rounded-xl py-2 px-1 bg-black text-[calc(1vw)] text-white hover:bg-pink hover:text-white hover:border-pink"
+            className="border border-black rounded-xl py-2 px-1 bg-black text-[calc(3vw)] md:text-[calc(1.5vw)] lg:text-[calc(1vw)] text-white hover:bg-pink hover:text-white hover:border-pink"
             href={'https://github.com/cocoabunnie'}
             target={'_blank'}
             rel="noreferrer"
@@ -21,7 +23,7 @@ export default function Header() {
             GitHub
           </a>
           <a
-            className="border border-black rounded-xl py-2 px-1 bg-black text-[calc(1vw)] text-white hover:bg-pink hover:text-white hover:border-pink"
+            className="border border-black rounded-xl py-2 px-1 bg-black text-[calc(3vw)] md:text-[calc(1.5vw)] lg:text-[calc(1vw)] text-white hover:bg-pink hover:text-white hover:border-pink"
             href={'https://www.linkedin.com/in/duvbri/'}
             target={'_blank'}
             rel="noreferrer"
@@ -31,10 +33,12 @@ export default function Header() {
         </div>
       </div>
 
-      <img
-        className="absolute top-60 right-0 w-[calc(40vw)] h-auto justify-end"
-        src="/images/selfIllustration.png"
+      <Image
+        className="absolute top-96 w-[calc(60vw)] md:top-72 lg:top-60 right-0 md:w-[calc(45vw)] h-auto justify-end"
+        src="/images/meOnABeanBag.png"
         alt=""
+        width={10000}
+        height={10000}
       />
     </div>
   )
