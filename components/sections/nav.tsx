2c1abc732896736500edd180746244dc7e5f36c0
Bri <duvivierbri@gmail.com>
2023-04-08 01:27:44 -0400

    Nav bar changes to back on other pages

--- components/sections/nav.tsx
+++ components/sections/nav.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 export default function NavigationBar() {
+  const router = useRouter()
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
@@ -14,9 +16,24 @@ export default function NavigationBar() {
         'font-mono fixed flex flex-row items-center w-screen z-[1] bg-black text-white'
       }
     >
-      <div className="flex grow justify-start items-center pl-10">
+      <div className="flex flex-row grow justify-start items-center pl-10">
+        {router.pathname !== '/' && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-2"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path d="M19 12H5M12 19l-7-7 7-7" />
+          </svg>
+        )}
+
         <Link className="uppercase" href="/">
-          Brianna Duvivier 🐶💕
+          {router.pathname === '/' ? 'Brianna Duvivier 🐶💕' : 'Back'}
         </Link>
       </div>
 
