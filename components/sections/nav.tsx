821e58de08ecb314a6bad1999aa2ebfb18314a60
Bri <duvivierbri@gmail.com>
2023-04-07 23:33:06 -0400

    Changed nav default value

--- components/sections/nav.tsx
+++ components/sections/nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 export default function NavigationBar() {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
