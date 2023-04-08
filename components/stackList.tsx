6e8a0822f9997e1f9fb8f874eb331162b722010a
Bri <duvivierbri@gmail.com>
2023-04-08 01:30:35 -0400

    More small styling

--- components/stackList.tsx
+++ components/stackList.tsx
@@ -1,7 +1,7 @@
 export default function StackList({ title, data }: any) {
   return (
     <div className="flex flex-col gap-5 justify-center items-center text-white">
-      <p className="text-[30px]">{title}</p>
+      <p className="text-[calc(5vw)] md:text-[30px]">{title}</p>
       {data.map((data: any) => {
         return (
           <div
@@ -9,7 +9,7 @@ export default function StackList({ title, data }: any) {
             className="flex flex-row items-center gap-2 w-fit h-fit"
           >
             <img className="w-10" src={data.imageSrc} alt="icon" />
-            <p className="text-[20px]">{data.name}</p>
+            <p className="text-[calc(4vw)] md:text-[20px]">{data.name}</p>
           </div>
         )
       })}
